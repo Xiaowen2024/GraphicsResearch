941ec43276560782157e7681a63e12786d6548ce
Xiaowen Yuan <xiaowen_yuan@berkeley.edu>
2024-11-14 12:08:13 -0800

    added strain stress trction calculation

--- solver/WostDeformationVector.cpp
+++ solver/WostDeformationVector.cpp
@@ -168,51 +168,6 @@ Vec2D solve( Vec2D x0, // evaluation point
    return Vec2D(sum_x/nWalks, sum_y/nWalks);
 }
 
-double solve_x( Vec2D x0, // evaluation point
-              vector<Polyline> boundaryDirichlet, // absorbing part of the boundary
-              vector<Polyline> boundaryNeumann, // reflecting part of the boundary
-              function<double(Vec2D)> g ) { // Dirichlet boundary values
-   const double eps = 0.0001; // stopping tolerance
-   const double rMin = 0.0001; // minimum step size
-   const int nWalks = 65536; // number of Monte Carlo samples
-   const int maxSteps = 65536; // maximum walk length
-
-   double sum = 0.0; // running sum of boundary contributions
-   // #pragma omp parallel for reduction(+:sum)
-   for( int i = 0; i < nWalks; i++ ) {
-      Vec2D x = x0; // start walk at the evaluation point
-      Vec2D n{ 0.0, 0.0 }; // assume x0 is an interior point, and has no normal
-      bool onBoundary = false; // flag whether x is on the interior or boundary
-
-      double r, dDirichlet, dSilhouette; // radii used to define star shaped region
-      int steps = 0;
-      do { // loop until the walk hits the Dirichlet boundary
-           
-         // compute the radius of the largest star-shaped region
-         dDirichlet = distancePolylines( x, boundaryDirichlet );
-         dSilhouette = silhouetteDistancePolylines( x, boundaryNeumann );
-         r = max( rMin, min( dDirichlet, dSilhouette ));
-
-         // intersect a ray with the star-shaped region boundary
-         double theta = random( -M_PI, M_PI );
-         if( onBoundary ) { // sample from a hemisphere around the normal
-            theta = theta/2. + angleOf(n);
-         }
-         Vec2D v{ cos(theta), sin(theta) }; // unit ray direction
-         x = intersectPolylines( x, v, r, boundaryNeumann, n, onBoundary );
-
-         steps++;
-      }
-      while(dDirichlet > eps && steps < maxSteps);
-      //stop if we hit the Dirichlet boundary, or the walk is too long
-
-      if( steps >= maxSteps ) cerr << "Hit max steps" << endl;
-
-      sum += g(x); // accumulate contribution of the boundary value
-   }
-   return sum/nWalks; // Monte Carlo estimate
-}
-
 
 
 // for simplicity, in this code we assume that the Dirichlet and Neumann
